7c1e401a91f2881943a7b66169dc2819ff954e6f
cendikiawanstudioindragiri <cendikiawanstudioindragiri@gmail.com>
2025-12-11 23:41:06 +0700

    Auto-Update: 2025-12-11 23:41:06

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Middleware agar bisa baca JSON
-app.use(express.json());
-
-// --- TAMBAHKAN BAGIAN INI ---
-const restaurantRoutes = require('./routes/restaurantRoutes');
-app.use('/api/restaurants', restaurantRoutes);
-// ----------------------------
-
-app.listen(port, () => {
-  console.log(`🚀 Server Backend dieHantar berjalan di port ${port}`);
-});
+const port = 3001; // <--- UBAH JADI INI
